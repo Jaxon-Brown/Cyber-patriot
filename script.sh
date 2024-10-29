1a383785236da1d8cff035aaf81246f9b4f991b5
Jaxon-Brown <148353156+Jaxon-Brown@users.noreply.github.com>
2024-10-29 11:07:18 -0400

    script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,33 @@
-read -p "Would you like to update packages: [Y]/[N]" response
+#!/bin/bash
 
-if [["$response" == "y" || "$response" == "Y"]]; 
-then 
-sudo apt-get update
-sudo apt-get upgrade
+# Run as root
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root (use sudo)"
+fi 
+
+echo "Updating the system..."
+apt update && apt upgrade -y
+
+if ! command -v ufw &> /dev/null; then 
+  echo "Installing UFW..."
+  apt install ufw -y
+else
+  echo "UFW is already installed."
+fi
+
+eco "Enabling UFW..."
+ufw enable
+
+echo "Allowing SSH..."
+ufw allow ssh 
+
+echo "Allowing HTTP..."
+ufw allow http 
+
+echo "Allowing HTTPS..."
+ufw allow https 
+
+echo "Checking UFW status..."
+ufw status verbose
+
+
