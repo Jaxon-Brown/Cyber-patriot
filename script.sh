d5250afb214e4128df262acdfe80cd0dd7394b41
Jaxon-Brown <148353156+Jaxon-Brown@users.noreply.github.com>
2024-10-29 11:48:35 -0400

    script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "Created by Jaxon Brown, Loudoun County High School, Leesburg, VA, USA"
 
+echo " "
+echo " "
 # Run as root
 if [ "$EUID" -ne 0 ]; then 
   echo "Please run as root (use sudo)"
@@ -15,7 +18,7 @@ else
   echo "UFW is already installed."
 fi
 
-eco "Enabling UFW..."
+echo "Enabling UFW..."
 ufw enable
 
 echo "Allowing SSH..."
@@ -30,4 +33,30 @@ ufw allow https
 echo "Checking UFW status..."
 ufw status verbose
 
+FILEA="/etc/login.defs"
 
+if [ ! -f "$FILEA" ]; then
+  echo "File $FILEA not found."
+fi 
+
+KEYA="PASS_MIN_DAYS 0"
+NEW_LINEA="PASS_MIN_DAYS 7"
+KEYB="PASS_MAX_DAYS"
+NEW_LINEB="PASS_MAX_DAYS 90"
+KEYC="PASS_WARN_AGE"
+NEW_LINEC="PASS_WARN_AGE 14"
+
+if grep -q "^$KEYA" "#FILEA"; then
+  echo "Updating existing parameter..."
+  sed -i "s|^KEYA.*|$NEW_LINEA|" "$FILEA"
+fi 
+
+if grep -q "^$KEYB" "#FILEA"; then
+  echo "Updating existing parameter..."
+  sed -i "s|^KEYB.*|$NEW_LINEB|" "$FILEA"
+fi 
+
+if grep -q "^$KEYC" "#FILEA"; then
+  echo "Updating existing parameter..."
+  sed -i "s|^KEYC.*|$NEW_LINEC|" "$FILEA"
+fi 
